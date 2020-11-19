71e4166ad0b1a757ef82d8018142a07611f26239
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-11-19 15:53:05 +0100

    dev(dev): enabling prehooks

--- path/to/jmeter/config/pre_hook.cli.ps1
+++ path/to/jmeter/config/pre_hook.cli.ps1
@@ -1,2 +1,2 @@
 Write-Host "HWhatever you put here is executed with armConnection rights"
-az aks nodepool list --resource-group jmeter-group --cluster-name k8
+az aks nodepool list --resource-group jmeter-group --cluster-name k8 --output table
